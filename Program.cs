34d40c9ba2def4385213ff80718bfdf067bcf550
Kauã <kau.lira@yahoo.com>
2022-03-18 22:50:05 -0300

    tarefa feita

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int n1, n2, resultado; 
+
+Console.WriteLine("Cálculo da soma entre dois números.\n");
+
+Console.Write("digite um número: ");
+n1 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("digite um número: ");
+n2 = Convert.ToInt32(Console.ReadLine()!);
+
+resultado = n1 + n2 ; 
+
+Console.WriteLine($"\n A soma dos números é: {resultado} ");
